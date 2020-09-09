8a7975ecf51eec03c26c1a1334f6b9e8b7b80e10
Jeyson Calvache <jeison130@gmail.com>
2020-09-09 04:00:08 +0000

    Agregando gunicorn para produccion

--- apiFileUpload/apiFileUpload/settings.py
+++ apiFileUpload/apiFileUpload/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = '=*h)*%nuiq4f0#!xm@ds3-#nwhx(vsk%h%&mc5t=^cyi#)e4bf'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '104.131.29.28'
+]
 
 
 # Application definition
@@ -124,6 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
